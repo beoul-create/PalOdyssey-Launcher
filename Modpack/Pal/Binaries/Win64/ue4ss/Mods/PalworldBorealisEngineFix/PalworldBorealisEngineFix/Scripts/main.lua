c660e5528d3003b305af1040bf4a08a6afba8260
beoul-create <beoul-create@users.noreply.github.com>
2026-08-22 00:30:21 -0500

    fix(modpack): clean single-level structure for ue4ss\Mods directory

--- Modpack/Pal/Binaries/Win64/ue4ss/Mods/PalworldBorealisEngineFix/PalworldBorealisEngineFix/Scripts/main.lua
+++
@@ -1,6 +0,0 @@
-﻿-- ============================================================================
--- Palworld Borealis Engine Unlocks (Clouds, Sky, Water)
--- Core graphics parameters are permanently embedded in Engine.ini [SystemSettings].
--- ============================================================================
-
-print("[BorealisEngineFix] Custom Volumetric & Water Graphics Active via Engine.ini.")
